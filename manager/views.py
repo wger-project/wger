fd7ae623bfa0de98f1140568d105ae57c00bc50e
Roland Geider <roland@geider.net>
2012-08-06 18:15:37 +0200

    Exercise overciew, simple add-form for workouts

--- manager/views.py
+++ manager/views.py
@@ -11,12 +11,25 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU Affero General Public License
+import logging
 
 from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.http import Http404
+from django.forms import ModelForm
+from django.core.context_processors import csrf
+
 from manager.models import TrainingSchedule
+from manager.models import Exercise
+
+
+logger = logging.getLogger('workout_manager.custom')
+
+class WorkoutForm(ModelForm):
+    class Meta:
+        model = TrainingSchedule
 
 
 def index(request):
@@ -25,8 +38,61 @@ def index(request):
     
 
 def add(request):
-    return render_to_response('add.html')
+    template_data = {}
+    template_data.update(csrf(request))
+    
+    if request.method == 'POST':
+        workout_form = WorkoutForm(request.POST)
+        new_workout = workout_form.save()
+        return HttpResponseRedirect('/workout/add/step/2')
+    else:
+        workout_form = WorkoutForm()
+    
+    exercises = Exercise.objects.all()
+    template_data['exercises'] = exercises
+    
+    
+    template_data['workout_form'] = workout_form
+    
+    return render_to_response('add.html', template_data)
 
+def add_step_2(request):
+    template_data = {}
+    
+    return render_to_response('add.html', template_data)
+
+def add_step_3(request):
+    template_data = {}
+    
+    return render_to_response('add.html', template_data)
+
+def add_step_4(request):
+    template_data = {}
+    
+    return render_to_response('add.html', template_data)
+
+
+
+
+def exercise_overview(request):
+    """Overview with all exercises
+    """
+    
+    template_data = {}
+    ex_data = {}
+    
+    # Gather all exercises and group them by category
+    for i in Exercise.objects.all().order_by('category'):
+        if not ex_data.get(i.category.id):
+            ex_data[i.category.id] = {'category': i.category, 'exercises': []}
+        
+        ex_data[i.category.id]['exercises'].append(i)
+
+    template_data['exercises'] = ex_data
+    logger.debug(template_data)
+    
+    return render_to_response('exersise/overview.html', template_data)
+    
 def view_workout(request, id):
     p = get_object_or_404(TrainingSchedule, pk=id)
     return render_to_response('detail.html', {'workout': p})
