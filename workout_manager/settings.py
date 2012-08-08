4596fe0d05af6dfa8bdefe08f6b23f721250e60d
Roland Geider <roland@geider.net>
2012-08-08 23:46:06 +0200

    Set LOGIN_URL

--- workout_manager/settings.py
+++ workout_manager/settings.py
@@ -20,6 +20,9 @@ DATABASES = {
     }
 }
 
+# Login-URL
+LOGIN_URL = '/login'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
