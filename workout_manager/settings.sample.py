80ef8aa660318e8a8d703c2538225640a6f9b04f
Roland Geider <roland@geider.net>
2012-08-21 23:06:44 +0200

    Add a sample settings file, remove the other one
    --HG--
    rename : workout_manager/settings.py => workout_manager/settings.sample.py

--- workout_manager/settings.sample.py
+++ workout_manager/settings.sample.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/media/multimedia/www/workout_manager/workout_manager/database.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/ogre/www/workout_manager/database.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
