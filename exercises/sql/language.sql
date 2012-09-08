7a84e89b578fc6599cc6a638571b4211e0ab645c
Roland Geider <roland@geider.net>
2012-09-08 15:50:52 +0200

    Move initial data from SQL to JSON, this is more database independent

--- exercises/sql/language.sql
+++
@@ -1,7 +0,0 @@
--- CREATE TABLE "exercises_language" (
---     "id" integer NOT NULL PRIMARY KEY,
---     "short_name" varchar(2) NOT NULL,
---     "full_name" varchar(30) NOT NULL
--- )
-INSERT INTO "exercises_language" VALUES(1,'de','Deutsch');
-INSERT INTO "exercises_language" VALUES(2,'en','English');
