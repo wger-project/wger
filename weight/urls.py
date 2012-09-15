ebfb520a2b59875ba2cca149b91cb5a8dc0bc9ef
Roland Geider <roland@geider.net>
2012-09-15 17:06:10 +0200

    Don't use hard-coded URL paths

--- weight/urls.py
+++ weight/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('weight.views',
-    url(r'^weight/add/(?P<id>\d*)$', 'add'),
+    url(r'^weight/add/(?P<id>\w*)$', 'add'),
     url(r'^weight/export_csv/$', 'export_csv'),
     url(r'^weight/overview/$', 'overview'),
     
