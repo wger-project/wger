0eeb10064f693a5be56e54222297589f5a595b41
Roland Geider <roland@geider.net>
2012-09-27 22:38:58 +0200

    The software is not called Foobar...

--- exercises/tests.py
+++ exercises/tests.py
@@ -1,6 +1,6 @@
 # This file is part of Workout Manager.
 # 
-# Foobar is free software: you can redistribute it and/or modify
+# Workout Manager is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
