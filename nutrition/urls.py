503a1ecff2faf1dba203764d62bd5f18c919c668
Roland Geider <roland@geider.net>
2012-10-04 09:27:41 +0200

    Make more URLs consistent to what is used elsewhere in the application

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -9,12 +9,11 @@ urlpatterns = patterns('nutrition.views',
     url(r'^nutrition/(?P<id>\d+)/delete/$', 'delete_plan'),
     url(r'^nutrition/(?P<id>\d+)/edit/$', 'edit_plan'),
     url(r'^nutrition/(?P<id>\d+)/pdf/$', 'export_pdf'),
-
     
     # Meals
     url(r'^nutrition/(?P<id>\d+)/edit/meal/(?P<meal_id>\w*)$', 'edit_meal'),
     url(r'^nutrition/(?P<id>\d+)/add/meal/$', 'add_meal'),
-    url(r'^nutrition/delete/meal/(?P<id>\d+)$', 'delete_meal'),
+    url(r'^nutrition/(?P<id>\d+)/delete/meal/$', 'delete_meal'),
     
     # Meal items
     url(r'^nutrition/(?P<id>\d+)/edit/meal/(?P<meal_id>\d+)/item/(?P<item_id>\w*)$', 'edit_meal_item'),
@@ -22,9 +21,8 @@ urlpatterns = patterns('nutrition.views',
     
     # Ingredients
     url(r'^nutrition/ingredient/overview/$', 'ingredient_overview'),
-    url(r'^nutrition/ingredient/view/(?P<id>\d+)$', 'ingredient_view'),
-    url(r'^nutrition/ingredient/edit/(?P<id>\w*)$', 'ingredient_edit'),
-    url(r'^nutrition/ingredient/delete/(?P<id>\d+)$', 'delete_ingredient'),
-    url(r'^nutrition/ingredient/search/$', 'ingredient_search'),
-    
+    url(r'^nutrition/ingredient/(?P<id>\d+)/view/$', 'ingredient_view'),
+    url(r'^nutrition/ingredient/(?P<id>\w*)/edit/$', 'ingredient_edit'),
+    url(r'^nutrition/ingredient/(?P<id>\d+)/delete/$', 'delete_ingredient'),
+    url(r'^nutrition/ingredient/search/$', 'ingredient_search'),   
 )
