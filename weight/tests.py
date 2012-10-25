e4427273ac48542302c42b1bc4f565e58f5929af
Roland Geider <roland@geider.net>
2012-10-25 22:17:01 +0200

    Add some tests for the weight application with selenium

--- weight/tests.py
+++ weight/tests.py
@@ -9,61 +9,100 @@ from django.test import TestCase
 from django.test import LiveServerTestCase
 from django.core.urlresolvers import reverse
 
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class WeigtTest(LiveServerTestCase):
-    fixtures = ['tests-user-data', 'test-exercises', ]
-    
+from manager.tests.testcase import WorkoutManagerLiveServerTestCase
 
-    def setUp(self):
-        """
-        Set up out testing browser
-        """
-        
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-        
-        
-    def tearDown(self):
-        """
-        Quit the browser
-        """
-        
-        self.browser.quit()
-        #pass
+class WeigtTest(WorkoutManagerLiveServerTestCase):
+
     
-    def user_login(self, user='admin'):
+    def test_weight_index_empty(self):
         """
-        Login the user
+        Tests that the weight overview page has no content if there are no
+        weight entries
         """
-        self.browser.get(self.live_server_url + reverse('manager.views.login'))
-        username_field = self.browser.find_element_by_id('id_username')
-        username_field.send_keys('admin')
         
-        password_field = self.browser.find_element_by_id('id_password')
-        password_field.send_keys('%(user)s%(user)s' % {'user': user})
-        password_field.send_keys(Keys.RETURN)
+        self.user_login()
+        self.browser.get(self.live_server_url + reverse('weight.views.overview'))
         
-    def user_logout(self, user='admin'):
-        """
-        Logout the user
-        """
-        self.browser.get(self.live_server_url + reverse('manager.views.logout'))
-        
-        
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.browser.get(self.live_server_url)
-        username_field = self.browser.find_element_by_id('id_username')
-        username_field.send_keys('admin')
-        
-        password_field = self.browser.find_element_by_id('id_password')
-        password_field.send_keys('adminadmin')
-        password_field.send_keys(Keys.RETURN)
-        
-        self.browser.get(self.live_server_url + '/weight/overview/')
+        # No diagram
         svg = self.browser.find_elements_by_tag_name('svg')
-        self.assertEqual(len(svg), 0)
+        self.assertFalse(svg)
+        
+        # Warning box
+        warning_box = self.browser.find_elements_by_id('weight-box-warning')
+        self.assertTrue(warning_box)
+
+    def add_weight_entry(self, weight=80, day_index=0):
+        """
+        Tests that it's possible to add a weight entry and that this appears
+        in the overview
+        """
+        
+        
+        
+        # Clink on the link and enter the weight in the modal dialog
+        add_entry_link = self.browser.find_elements_by_id('add-weight-entry')
+        self.assertTrue(add_entry_link)
+        
+        add_entry_link[0].click()
+        
+        modal_dialog = self.browser.find_elements_by_class_name('ui-dialog')[0]
+        self.assertTrue(modal_dialog)
+        
+        # Found the title
+        dialog_title = modal_dialog.find_elements_by_id('ui-id-1')
+        self.assertTrue(dialog_title)
+        
+        # Found the content
+        dialog_content = modal_dialog.find_elements_by_id('ajax-info')[0]
+        self.assertTrue(dialog_content)
+        
+        # Put some number into the weight input
+        weight_input = modal_dialog.find_elements_by_id('id_weight')[0]
+        self.assertTrue(weight_input)
+        weight_input.send_keys(weight)
+        
+        # Focus on the date, a date picker should appear, select something
+        date_input = modal_dialog.find_elements_by_id('id_creation_date')[0]
+        self.assertTrue(date_input)
+        date_input.click()
+        datepicker_div = self.browser.find_elements_by_id('ui-datepicker-div')[0]
+        day = datepicker_div.find_elements_by_tag_name('tbody')[0].find_elements_by_tag_name('a')[day_index]
+        day.click()
+        
+        save_button = dialog_content.find_element_by_id('form-save')
+        save_button.click()
+    
+    def test_add_weight_entry(self):
+        """
+        Tests that it's possible to add a weight entry and that this appears
+        in the overview
+        """
+        
+        self.user_login()
+        self.browser.get(self.live_server_url + reverse('weight.views.overview'))
+        self.add_weight_entry()
+    
+    def test_weight_index_data(self):
+        """
+        Tests that after adding weight entries, there is a diagram generated
+        """
+        
+        self.user_login()
+        self.browser.get(self.live_server_url + reverse('weight.views.overview'))
+        self.add_weight_entry(70, 0)
+        self.add_weight_entry(71, 1)
+        self.add_weight_entry(76, 2)
+        self.add_weight_entry(80, 3)
+        self.add_weight_entry(86, 4)
+        self.add_weight_entry(81, 5)
+        self.add_weight_entry(83, 7)
+        
+        # There is a diagram with 8 data points
+        svg_points = self.browser.find_elements_by_tag_name('circle')
+        self.assertTrue(len(svg_points), 8)
+        
+        # There is no warning box anymore
+        warning_box = self.browser.find_elements_by_id('weight-box-warning')
+        self.assertFalse(warning_box)
