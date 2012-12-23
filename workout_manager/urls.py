9541fd723308d51f7c380649a81b4992074a1193
Roland Geider <roland@geider.net>
2012-12-23 18:14:46 +0100

    Append the name of the current language to the URLs
    --HG--
    branch : 1.1-dev

--- workout_manager/urls.py
+++ workout_manager/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = i18n_patterns('',
     url(r'^admin/',   include(admin.site.urls)),
     url(r'^', include('manager.urls')),
     url(r'exercise/', include('exercises.urls')),
