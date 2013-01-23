99534bad996df0ad86039ab43f095b31b3430ea3
Roland Geider <roland@geider.net>
2013-01-23 18:06:03 +0100

    Don't enconde entities with tinyMCE, save them raw in the DB
    --HG--
    branch : 1.1-dev

--- wger/manager/static/js/workout-manager.js
+++ wger/manager/static/js/workout-manager.js
@@ -214,7 +214,8 @@ function init_tinymce() {
             mode : "textareas",
             theme : "simple",
             width : "100%",
-            height : "200"
+            height : "200",
+            entity_encoding : "raw"
         });
    }
 }
