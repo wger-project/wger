5014bbf5e88ed78f40f730b49bb681eb898205f7
Roland Geider <roland@geider.net>
2013-01-25 22:20:10 +0100

    Update manage.py
    --HG--
    branch : 1.1-dev

--- manage.py
+++ manage.py
@@ -2,9 +2,14 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.workout_manager.settings")
+from django.core.management import execute_from_command_line
 
-    from django.core.management import execute_from_command_line
+from wger.main import get_user_config_path, setup_django_environment
+
+if __name__ == "__main__":
+    setup_django_environment(
+        get_user_config_path('wger', 'settings.py'))
+        
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.workout_manager.settings")
 
     execute_from_command_line(sys.argv)
