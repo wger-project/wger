74d8d9abec03ca4b5d3170e594a6bffa85819b80
Roland Geider <roland@geider.net>
2013-07-08 22:18:27 +0200

    Add tests for the workout template cache

--- wger/manager/tests/workout.py
+++ wger/manager/tests/workout.py
@@ -15,12 +15,17 @@
 import datetime
 
 from django.core.urlresolvers import reverse
+from django.core.cache import cache
 
 from wger.manager.models import Workout
+from wger.manager.models import Day
+from wger.manager.models import Set
+from wger.manager.models import Setting
 
 from wger.manager.tests.testcase import WorkoutManagerTestCase
 from wger.manager.tests.testcase import WorkoutManagerDeleteTestCase
 from wger.manager.tests.testcase import WorkoutManagerEditTestCase
+from wger.utils.cache import get_template_cache_name
 
 
 class AddWorkoutTestCase(WorkoutManagerTestCase):
@@ -128,3 +133,23 @@ class WorkoutModelTestCase(WorkoutManagerTestCase):
 
         workout.comment = u'my description'
         self.assertEqual(workout.__unicode__(), u'my description')
+
+
+class WorkoutCacheTestCase(WorkoutManagerTestCase):
+    '''
+    Workout cache test case
+    '''
+
+    def test_workout_view_day(self):
+        '''
+        Test the workout view cache is correctly generated on visit
+        '''
+
+        self.user_login('admin')
+        self.assertFalse(cache.get(get_template_cache_name('day-view', 1)))
+        self.assertFalse(cache.get(get_template_cache_name('day-view', 2)))
+
+        self.client.get(reverse('wger.manager.views.workout.view', kwargs={'id': 1}))
+
+        self.assertTrue(cache.get(get_template_cache_name('day-view', 1)))
+        self.assertTrue(cache.get(get_template_cache_name('day-view', 2)))
