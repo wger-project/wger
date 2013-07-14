e698858728ba69138670d5a0a7610b8f88cfbcc1
Roland Geider <roland@geider.net>
2013-07-14 10:57:41 +0200

    Fix unicode error

--- wger/nutrition/views/plan.py
+++ wger/nutrition/views/plan.py
@@ -237,7 +237,7 @@ def export_pdf(request, id):
                 unit_name = 'g'
             else:
                 unit_name = ' ' + item.weight_unit.unit.name
-            data.append(["{0}{1}".format(item.amount, unit_name), P])
+            data.append([u"{0}{1}".format(item.amount, unit_name), P])
 
     # Set general table styles
 
