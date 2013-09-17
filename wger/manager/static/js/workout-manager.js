979427fdf568991a8d9ef80d0adf5e4bce239255
Roland Geider <roland@geider.net>
2013-09-17 18:47:25 +0200

    Use the slidestop event to update the formsets for set add form
    The change event is fired many times when using the slider. The
    slidestop as the name implies happens only once, after releasing.
    
    --HG--
    branch : mobile

--- wger/manager/static/js/workout-manager.js
+++ wger/manager/static/js/workout-manager.js
@@ -336,12 +336,13 @@ function hex_random()
 function add_exercise(exercise)
 {
     var result_div = '<div id="DIV-ID" class="ajax-exercise-select"> \
-<a href="#"> \
+<a href="#" data-role="button"> \
 <img src="/static/images/icons/status-off.svg" \
      width="14" \
      height="14" \
      alt="Delete"> \
-</a> EXERCISE \
+EXERCISE \
+</a> \
 <input type="hidden" name="exercises" value="EXCERCISE-ID"> \
 </div>';
 
@@ -352,7 +353,7 @@ function add_exercise(exercise)
 
     $(result_div).prependTo("#exercise-search-log");
     $("#exercise-search-log").scrollTop(0);
-
+    $('#exercise-search-log').trigger('create');
 }
 
 function get_exercise_formset(exercise_id)
