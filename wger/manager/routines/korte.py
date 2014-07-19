891f793aa27053ee4f852866bc91e2f7241222e1
Roland Geider <roland@geider.net>
2014-07-19 17:41:31 +0200

    Fix Korte 3x3 routine data

--- wger/manager/routines/korte.py
+++ wger/manager/routines/korte.py
@@ -21,62 +21,193 @@ from wger.utils.routines import ExerciseConfig
 
 logger = logging.getLogger('wger.custom')
 
-
-class KorteRoutine(Routine):
-    pass
+MAX_SQUAT = 100
+MAX_BENCH = 110
+MAX_DEADLIFT = 120
 
 
 class SquatConfig(ExerciseConfig):
+
     data = {
-        1: {1: [2, 3, 4],
-            2: [1]},
-        2: {1: [1, 2],
-            3: [5, 6]}
+        1: {1: [1, 2, 3, 4, 5],
+            2: [1, 2, 3, 4, 5],
+            3: [1, 2, 3, 4, 5]},
+        2: {1: [1, 2, 3, 4, 5],
+            2: [1, 2, 3, 4, 5],
+            3: [1, 2, 3, 4, 5]},
+        3: {1: [1, 2, 3, 4, 5],
+            2: [1, 2, 3, 4, 5],
+            3: [1, 2, 3, 4, 5]},
+        4: {1: [1, 2, 3, 4, 5],
+            2: [1, 2, 3, 4, 5],
+            3: [1, 2, 3, 4, 5]},
+        5: {1: [1, 2, 3],
+            2: [1, 2, 3],
+            3: [1]},
+        6: {1: [1, 2, 3],
+            2: [1, 2, 3],
+            3: [1]},
+        7: {1: [1, 2, 3],
+            2: [1, 2, 3],
+            3: [1]},
+        8: {1: [1, 2, 3],
+            2: [1, 2, 3],
+            3: [1]},
     }
 
     def get_routine(self):
         if self.current_week == 1:
-            if self.current_day == 1:
-                if self.current_set == 2:
-                    return 8, 100
-                elif self.current_set == 3:
-                    return 3, 150
-                elif self.current_set == 4:
-                    return 5, 120
-            elif self.current_day == 2:
-                return 5, 125
+            return 5, (MAX_SQUAT + 11) * 0.58
         elif self.current_week == 2:
-            return 3, 120
+            return 5, (MAX_SQUAT + 11) * 0.6
+        elif self.current_week == 3:
+            return 5, (MAX_SQUAT + 11) * 0.62
+        elif self.current_week == 4:
+            return 5, (MAX_SQUAT + 11) * 0.64
+        elif self.current_week == 5:
+            if self.current_day != 3:
+                return 3, (MAX_SQUAT + 11) * 0.6
+            else:
+                return 1, (MAX_SQUAT + 11) * 0.80
+        elif self.current_week == 6:
+            if self.current_day != 3:
+                return 3, (MAX_SQUAT + 11) * 0.6
+            else:
+                return 1, (MAX_SQUAT + 11) * 0.85
+        elif self.current_week == 7:
+            if self.current_day != 3:
+                return 3, (MAX_SQUAT + 11) * 0.6
+            else:
+                return 1, (MAX_SQUAT + 11) * 0.90
+        elif self.current_week == 8:
+            if self.current_day != 3:
+                return 3, (MAX_SQUAT + 11) * 0.6
+            else:
+                return 1, (MAX_SQUAT + 11) * 0.95
 
 
 class BenchConfig(ExerciseConfig):
     data = {
-        1: {1: [1],
-            2: [2, 3, 4, 6, 7]},
-        2: {1: [3, 4],
-            2: [1, 2, 3, 4, 5, 6]}
+        1: {1: [6, 7, 8, 9, 10],
+            2: [6, 7, 8, 9, 10],
+            3: [6, 7, 8, 9, 10]},
+        2: {1: [6, 7, 8, 9, 10],
+            2: [6, 7, 8, 9, 10],
+            3: [6, 7, 8, 9, 10]},
+        3: {1: [6, 7, 8, 9, 10],
+            2: [6, 7, 8, 9, 10],
+            3: [6, 7, 8, 9, 10]},
+        4: {1: [6, 7, 8, 9, 10],
+            2: [6, 7, 8, 9, 10],
+            3: [6, 7, 8, 9, 10]},
+        5: {1: [4, 5, 6, 7, 8],
+            2: [4],
+            3: [2, 3, 4, 5, 6]},
+        6: {1: [4, 5, 6, 7, 8],
+            2: [4],
+            3: [2, 3, 4, 5, 6]},
+        7: {1: [4, 5, 6, 7, 8],
+            2: [4],
+            3: [2, 3, 4, 5, 6]},
+        8: {1: [4, 5, 6, 7, 8],
+            2: [4],
+            3: [2, 3, 4, 5, 6]},
     }
 
     def get_routine(self):
-        return 5, 100
+        if self.current_week == 1:
+            return 5, (MAX_BENCH + 4) * 0.58
+        elif self.current_week == 2:
+            return 5, (MAX_BENCH + 4) * 0.6
+        elif self.current_week == 3:
+            return 5, (MAX_BENCH + 4) * 0.62
+        elif self.current_week == 4:
+            return 5, (MAX_BENCH + 4) * 0.64
+        elif self.current_week == 5:
+            if self.current_day != 2:
+                return 4, (MAX_BENCH + 4) * 0.6
+            else:
+                return 1, (MAX_BENCH + 4) * 0.80
+        elif self.current_week == 6:
+            if self.current_day == 2:
+                return 1, (MAX_BENCH + 4) * 0.85
+            else:
+                return 4, (MAX_BENCH + 4) * 0.6
+        elif self.current_week == 7:
+            if self.current_day == 2:
+                return 1, (MAX_BENCH + 4) * 0.90
+            else:
+                return 4, (MAX_BENCH + 4) * 0.6
+        elif self.current_week == 8:
+            if self.current_day == 2:
+                return 1, (MAX_BENCH + 4) * 0.95
+            else:
+                return 4, (MAX_BENCH + 4) * 0.6
 
 
 class DeadliftConfig(ExerciseConfig):
     data = {
-        1: {1: [5, 6, 7, 8, 9],
-            2: [5]},
-        2: {1: [5, 6],
-            3: [1, 2, 3, 4]}
+        1: {1: [11, 12, 13, 14, 15],
+            2: [11, 12, 13, 14, 15],
+            3: [11, 12, 13, 14, 15]},
+        2: {1: [11, 12, 13, 14, 15],
+            2: [11, 12, 13, 14, 15],
+            3: [11, 12, 13, 14, 15]},
+        3: {1: [11, 12, 13, 14, 15],
+            2: [11, 12, 13, 14, 15],
+            3: [11, 12, 13, 14, 15]},
+        4: {1: [11, 12, 13, 14, 15],
+            2: [11, 12, 13, 14, 15],
+            3: [11, 12, 13, 14, 15]},
+        5: {1: [9],
+            2: [5, 6, 7],
+            3: [7, 8, 9]},
+        6: {1: [9],
+            2: [5, 6, 7],
+            3: [7, 8, 9]},
+        7: {1: [9],
+            2: [5, 6, 7],
+            3: [7, 8, 9]},
+        8: {1: [9],
+            2: [5, 6, 7],
+            3: [7, 8, 9]},
     }
 
     def get_routine(self):
-        return 5, 200
+        if self.current_week == 1:
+            return 5, (MAX_DEADLIFT + 6) * 0.58
+        elif self.current_week == 2:
+            return 5, (MAX_DEADLIFT + 6) * 0.6
+        elif self.current_week == 3:
+            return 5, (MAX_DEADLIFT + 6) * 0.62
+        elif self.current_week == 4:
+            return 5, (MAX_DEADLIFT + 6) * 0.64
+        elif self.current_week == 5:
+            if self.current_day == 1:
+                return 1, (MAX_DEADLIFT + 6) * 0.80
+            else:
+                return 3, (MAX_DEADLIFT + 6) * 0.60
+        elif self.current_week == 6:
+            if self.current_day == 1:
+                return 1, (MAX_DEADLIFT + 6) * 0.85
+            else:
+                return 3, (MAX_DEADLIFT + 6) * 0.60
+        elif self.current_week == 7:
+            if self.current_day == 1:
+                return 1, (MAX_DEADLIFT + 6) * 0.90
+            else:
+                return 3, (MAX_DEADLIFT + 6) * 0.60
+        elif self.current_week == 8:
+            if self.current_day == 1:
+                return 1, (MAX_DEADLIFT + 6) * 0.95
+            else:
+                return 3, (MAX_DEADLIFT + 6) * 0.60
 
 
-korte = KorteRoutine(weeks=8,
-                     name='Korte 3x3',
-                     description=_("Pure German volume training (GVT). You will squat, "
-                                   "bench, and deadlift. That's it. Good luck."))
+korte = Routine(weeks=8,
+                name='Korte 3x3',
+                description=_("Pure German volume training (GVT). You will squat, "
+                              "bench, and deadlift. That's it. Good luck."))
 
 e1 = SquatConfig('Squats', None)
 e2 = BenchConfig('Bench', None)
