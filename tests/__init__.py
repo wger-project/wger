7fd4454544cd87450eece8f7a5869ef07281fca1
Roland Geider <roland@geider.net>
2014-08-15 12:49:20 +0200

    Remove unneeded 'tests' folder
    The tests are all in the individual apps folder

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from wger.exercises.tests import *
-from wger.manager.tests import *
-from wger.weight.tests import *
-from wger.nutrition.tests import *
-from wger.config.tests import *
-from wger.utils.tests import *
