440a5ee93521ed1de82f5a79d5a021ce5f90e9a5
Roland Geider <roland@geider.net>
2014-08-21 15:57:11 +0200

    Bump version of tested django version to 1.6.6

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@
     :license: GNU GPL, see LICENSE for more details.
 """
 
-# for python 2.5 support
-from __future__ import with_statement
-
 from setuptools import setup
 from setuptools import find_packages
 from wger import get_version
@@ -43,7 +40,7 @@ setup(
         'versiontools >= 1.6',
     ],
     install_requires=[
-        'django == 1.6.1',
+        'django == 1.6.6',
         'reportlab',
         'django-browserid == 0.9',
         'django-recaptcha',
