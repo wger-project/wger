9ebba79c6d972f5a5a303673e1adac650e49b4c5
Roland Geider <roland@geider.net>
2014-11-23 10:43:00 +0100

    Add dependency on gym migration file

--- wger/core/migrations/0008_auto__add_field_gym_phone.py
+++ wger/core/migrations/0008_auto__add_field_gym_phone.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('config', '0003_auto__add_gymconfig__chg_field_languageconfig_language_target__chg_fie'),
+    )
+
     def forwards(self, orm):
         # Adding field 'Gym.phone'
         db.add_column(u'core_gym', 'phone',
