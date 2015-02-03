40905893c296e2c812539079925adfd25e39d44f
Roland Geider <roland@geider.net>
2015-02-03 09:59:33 +0100

    Change location of default settings in WSGI

--- wger/wsgi.py
+++ wger/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workout_manager.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
