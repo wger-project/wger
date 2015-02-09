e39829869a96ffc3c32c53cfc35a512262c67939
Roland Geider <roland@geider.net>
2015-02-09 17:38:06 +0100

    Add ordering information to m2m set data
    Exercises that are added as a super set where not properly sorted, as the
    intermediate many to many table had no sorting information.
    
    Fixes #89

--- wger/manager/models.py
+++ wger/manager/models.py
@@ -29,6 +29,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.exceptions import ValidationError
 from django.core.cache import cache
 from django.core.validators import MinValueValidator
+from sortedm2m.fields import SortedManyToManyField
 
 from wger.core.models import DaysOfWeek
 from wger.exercises.models import Exercise
@@ -508,8 +509,8 @@ class Set(models.Model):
 
     exerciseday = models.ForeignKey(Day,
                                     verbose_name=_('Exercise day'))
-    exercises = models.ManyToManyField(Exercise,
-                                       verbose_name=_('Exercises'))
+    exercises = SortedManyToManyField(Exercise,
+                                      verbose_name=_('Exercises'))
     order = models.IntegerField(blank=True,
                                 null=True,
                                 verbose_name=_('Order'))
