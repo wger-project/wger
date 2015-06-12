b2cf08ba36a7540f5950267d2b832397578ff444
Roland Geider <roland@geider.net>
2015-06-12 12:42:09 +0200

    Don't use a string as a dictionary key

--- wger/manager/views/workout.py
+++ wger/manager/views/workout.py
@@ -250,7 +250,7 @@ class LastWeightHelper():
         :param reps:
         :return: WorkoutLog or '' if none is found
         '''
-        key = u'{0}-{1}-{2}'.format(self.user.pk, exercise.pk, reps)
+        key = (self.user.pk, exercise.pk, reps)
 
         if self.last_weight_list.get(key) is None:
             last_log = WorkoutLog.objects.filter(user=self.user,
