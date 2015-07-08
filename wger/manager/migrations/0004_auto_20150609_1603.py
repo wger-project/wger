efb88321235cefd4f3bf9715ac15580a766ac591
Roland Geider <roland@geider.net>
2015-07-08 14:37:44 +0200

    Fix dependency of migration

--- wger/manager/migrations/0004_auto_20150609_1603.py
+++ wger/manager/migrations/0004_auto_20150609_1603.py
@@ -8,11 +8,11 @@ import django.core.validators
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('manager', '0003_auto_20150518_1534'),
+        ('manager', '0002_auto_20150202_2040'),
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='setting',
             name='weight',
             field=models.DecimalField(decimal_places=2, validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(1500)], max_digits=6, blank=True, null=True, verbose_name='Weight'),
