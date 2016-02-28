47797245bf8cc183e48b6465f1b8f3ca470e334a
Roland Geider <roland@geider.net>
2016-02-28 17:00:45 +0100

    Fix system path in manage.py
    The tasks.py file changes it due to some packaging problems. Since some of its
    helper functions are imported here, we need to fix that.

--- manage.py
+++ manage.py
@@ -10,6 +10,11 @@ from wger.tasks import (
 
 if __name__ == "__main__":
 
+    # Add the current directory to the system path. This is needed because
+    # tasks.py removes it, read the comment the on why this clutch is needed.
+    # Otherwise "local" setting files could not be imported
+    sys.path.append('.')
+
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
         setup_django_environment(get_user_config_path('wger', 'settings.py'))
