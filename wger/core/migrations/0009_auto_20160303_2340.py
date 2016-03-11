b2848203269c0b84d6cff4ba5a422c39da613582
Roland Geider <roland@geider.net>
2016-03-11 20:15:06 +0100

    Protext repetition unit 2 "until failure"
    This repetition should also not be deletable, since its ID will be hard coded
    in some places of the application.

--- wger/core/migrations/0009_auto_20160303_2340.py
+++ wger/core/migrations/0009_auto_20160303_2340.py
@@ -12,19 +12,20 @@ def insert_data(apps, schema_editor):
     fixtures or perform any intermediate steps.
     '''
     WeightUnit = apps.get_model('core', 'WeightUnit')
-    WeightUnit(name='Kg').save()
-    WeightUnit(name='Lb').save()
+    WeightUnit(name='kg').save()
+    WeightUnit(name='lb').save()
     WeightUnit(name='Body Weight').save()
     WeightUnit(name='Plates').save()
     WeightUnit(name='Kilometers Per Hour').save()
     WeightUnit(name='Miles Per Hour').save()
 
     RepetitionUnit = apps.get_model('core', 'RepetitionUnit')
-    WeightUnit(name='Repetitions').save()
-    WeightUnit(name='Seconds').save()
-    WeightUnit(name='Minutes').save()
-    WeightUnit(name='Miles').save()
-    WeightUnit(name='Kilometers').save()
+    RepetitionUnit(name='Repetitions').save()
+    RepetitionUnit(name='Until Failure').save()
+    RepetitionUnit(name='Seconds').save()
+    RepetitionUnit(name='Minutes').save()
+    RepetitionUnit(name='Miles').save()
+    RepetitionUnit(name='Kilometers').save()
 
 
 class Migration(migrations.Migration):
