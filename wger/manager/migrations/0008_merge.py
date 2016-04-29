519e28e433e2e8b1e7ff0ade34da9e1144de8de6
Roland Geider <roland@geider.net>
2016-04-29 16:24:37 +0200

    Commit missing migration merge

---
+++ wger/manager/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('manager', '0007_auto_20160311_2258'),
+        ('manager', '0005_auto_20150812_1554'),
+    ]
+
+    operations = [
+    ]
