9527911ca6bba4ba7e90ffdc5b07115082e72b03
Roland Geider <roland@geider.net>
2016-05-29 17:42:28 +0200

    Move JS weight functions to own file
    This reduces the amount of inline JS and makes doing changes easier.

---
+++ wger/weight/static/js/weight.js
@@ -0,0 +1,67 @@
+/*
+ * This file is part of wger Workout Manager.
+ *
+ * wger Workout Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wger Workout Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
+"use strict";
+
+$(document).ready(function () {
+
+    var weight_chart = {};
+    var chart_params = {
+        animate_on_load: true,
+        full_width: true,
+        height: 300,
+        show_secondary_x_label: true,
+        xax_count: 10,
+        target: '#weight_diagram',
+        x_accessor: 'date',
+        y_accessor: 'weight'
+    }
+
+
+    var username = $('#current-username').data('currentUsername');
+    var url = "/weight/api/get_weight_data/" + username;
+
+    d3.json(url, function (json) {
+
+        var data =  MG.convert.date(json, 'date');
+        weight_chart.data = data;
+
+        // Plot the data
+        chart_params.data = data;
+        MG.data_graphic(chart_params);
+    });
+
+    $('.modify-time-period-controls button').click(function () {
+        var past_n_days = $(this).data('time_period');
+        var data = modify_time_period(weight_chart.data, past_n_days);
+
+        // change button state
+        $(this).addClass('active').siblings().removeClass('active');
+
+        chart_params.data = data;
+        MG.data_graphic(chart_params);
+    });
+});
+
+
+function modify_time_period(data, past_n_days) {
+    if (past_n_days !== 'all' || past_n_days !== '') {
+        return MG.clone(data).slice(past_n_days * -1);
+    }
+
+    return data;
+}
+
