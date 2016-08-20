6ae77e736c1ca88b44aa059160ed353737e66907
Peter van der Does <peter@avirtualhome.com>
2016-08-20 15:25:55 -0400

    Start linting all javascript.
    Skip javascript we don't want to lint, things that are installed
    through npm, bower, and 3rd party javascript.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('lint', function () {
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src(['wger/nutrition/static/js/nutrition.js','!node_modules/**'])
+    return gulp.src(['**/*.js'])
         // eslint() attaches the lint output to the "eslint" property
         // of the file object so it can be used by other modules.
         .pipe(eslint())
