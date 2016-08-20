1e14a1d56288b58917a4e130835e1e6c8eb47a9f
Peter van der Does <peter@avirtualhome.com>
2016-08-19 23:58:21 -0400

    Run lint through gulp.
    Currently only one file is processed, we'll do all files at a later
    time.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
 
-gulp.task('default', function _default() {
-  // place code for your default task here
+gulp.task('lint', function () {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['wger/nutrition/static/js/nutrition.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], function () {
+    // This will only run if the lint task is successful...
 });
