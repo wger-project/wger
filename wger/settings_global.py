6e0ea9f44192607429111adb0c17a3ad17ca7a14
Peter van der Does <peter@avirtualhome.com>
2017-04-29 14:54:30 -0400

    Bower can't be found

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -333,7 +333,7 @@ COMPRESS_ROOT = STATIC_ROOT
 if sys.platform.startswith('win32'):
     BOWER_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', 'node_modules', '.bin', 'bower.cmd'))
 else:
-    BOWER_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', 'node_modules', '.bin', 'bower'))
+    BOWER_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', 'node_modules', 'bin', 'bower'))
 
 #
 # Django Rest Framework
