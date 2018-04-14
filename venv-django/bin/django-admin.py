31867af6bb7a7df1f554376632d18c1ed2f767fa
Jason <jpinho@umich.edu>
2018-04-14 15:04:29 -0400

    removed venv-django/ folder that was previously committed to my fork on accident

--- venv-django/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jason/Desktop/EECS481/Homeworks/HW6/wger/venv-django/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
