53fed92178c4149b06d0c25c714a97f3ad8516d9
Scott Peshak <scott@peshak.net>
2019-05-29 06:51:58 -0500

    Install django_extensions

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.staticfiles',
+    'django_extensions',
 
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
