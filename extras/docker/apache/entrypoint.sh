4ea4ec6f47aaaf445aa6a3a9ced8397062ab7df0
Scott Peshak <scott@peshak.net>
2019-06-18 20:17:17 -0500

    Force compression, install migrations in docker entrypoint

--- extras/docker/apache/entrypoint.sh
+++ extras/docker/apache/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+/home/wger/venvwrapper migrate
+
 if [ "${USE_S3}" = "TRUE" ]; then
   /home/wger/venvwrapper collectstatic --no-input
-  /home/wger/venvwrapper compress
+  /home/wger/venvwrapper compress --force
 fi
 
 /usr/sbin/apache2ctl -D FOREGROUND
