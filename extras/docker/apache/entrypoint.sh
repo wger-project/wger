9f1abb125abba4a94ea342cfb86ca75ebdf45b3b
Scott Peshak <scott@peshak.net>
2019-06-20 07:44:54 -0500

    Move image download to container startup
    Optional operation controlled by the DOWNLOAD_IMGS environmental
    variable

--- extras/docker/apache/entrypoint.sh
+++ extras/docker/apache/entrypoint.sh
@@ -2,6 +2,11 @@
 
 /home/wger/venvwrapper migrate
 
+if [ "${DOWNLOAD_IMGS}" = "TRUE" ]; then
+  /home/wger/venvwrapper download-exercise-images
+  chmod -R g+w ~wger/media
+fi
+
 if [ "${USE_S3}" = "TRUE" ]; then
   /home/wger/venvwrapper collectstatic --no-input
   /home/wger/venvwrapper compress --force
