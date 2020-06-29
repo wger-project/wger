e6f5a2eaafd2d7587330c0ae94cd2dbab8dfe970
Roland <roland@geider.net>
2020-06-30 01:18:16 +0200

    Fix workout detail page option dropdown

--- setup.py
+++ setup.py
@@ -49,38 +49,7 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
 
-    # Copy-pasting here the requirement since the git+git:// syntax is not
-    # supported here. After the release, django-user-agent will be removed
-    # and this can be replaced again with: install_requires=install_requires,
-    install_requires=[
-            "wheel",
-            "bleach>=3.1,<3.2",
-            "django-bootstrap-breadcrumbs==0.9.1",
-            "django-bower==5.2.0",
-            "django-formtools>=2.0,<3.0",
-            "django-recaptcha==2.0.6",
-            "Django>=3.0,<3.1",
-            "django_compressor>=2.4,<2.5",
-            "django_extensions>=2.1",
-            "django-sortedm2m>=3.0,<3.1",
-            "django-storages>=1.7",
-            "easy-thumbnails>=2.7,<2.8",
-            "icalendar==4.0.3",
-            "invoke==0.17",
-            "pillow>=6.2,<6.3",
-            "python-mimeparse",
-            "reportlab==3.5.21",
-            "matplotlib>=3.1",
-            "requests",
-            "setuptools>=18.5",
-            "sphinx",
-
-            "django-cors-headers>=3.0",
-            "django-filter==2.1.0",
-            "django-tastypie>=0.14,<.0.15",
-            "djangorestframework>=3.11,<3.12",
-            "django-user_agents"
-        ],
+    nstall_requires=install_requires,
     entry_points={
         'console_scripts': [
             'wger = wger.__main__:main',
