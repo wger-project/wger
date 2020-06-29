62100019a51f4faa4684cd82b9d0974021a97683
Roland <roland@geider.net>
2020-06-30 00:11:06 +0200

    Start migration to bootstrap 4

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -92,7 +92,7 @@ INSTALLED_APPS = (
 
 # added list of external libraries to be installed by bower
 BOWER_INSTALLED_APPS = (
-    'bootstrap#3.x',
+    'bootstrap#4.x',
     'components-font-awesome#4.7.x',
     'd3',
     'DataTables',
