dc297636510a6c5ec69305c8bc7da8ebd4d7bf4c
Roland Geider <roland@geider.net>
2020-08-11 14:20:39 +0200

    Remove unused field rendering tags
    All this is now done with crispy forms

--- wger/core/templatetags/wger_extras.py
+++ wger/core/templatetags/wger_extras.py
@@ -17,10 +17,6 @@
 # Django
 from django import template
 from django.conf import settings
-from django.forms.widgets import (
-    CheckboxInput,
-    ClearableFileInput
-)
 from django.utils.html import strip_spaces_between_tags
 from django.utils.safestring import mark_safe
 from django.utils.translation import (
@@ -32,10 +28,6 @@ from django.utils.translation import (
 from wger.utils.constants import (
     PAGINATION_MAX_TOTAL_PAGES,
     PAGINATION_PAGES_AROUND_CURRENT
-)
-from wger.utils.widgets import (
-    BootstrapSelectMultiple,
-    BootstrapSelectMultipleTranslatedOriginal
 )
 
 
@@ -137,19 +129,6 @@ def get_item(dictionary, key):
 
 
 @register.simple_tag
-def auto_link_css(mobile=False, css=''):
-    """
-    Adds the appropriate classes to a sidebar link depending on the site version
-
-    :param mobile: true if mobile version of site
-    :param css: the CSS class, if any, of the link
-    :return: the complete CSS classes, wrapped in class="foo"
-    """
-    css = css + ' btn btn-default btn-block' if mobile else css
-    return mark_safe('class="{0}"'.format(css))
-
-
-@register.simple_tag
 def fa_class(class_name='', icon_type='fas', fixed_width=True):
     """
     Helper function to help add font awesome classes to elements
@@ -224,121 +203,3 @@ def spaceless_config(parser, token):
     nodelist = parser.parse(('endspaceless_config',))
     parser.delete_first_token()
     return SpacelessNode(nodelist)
-
-
-#
-# Form utils
-#
-@register.filter(name='form_field_add_css')
-def form_field_add_css(field, css):
-    """
-    Adds a CSS class to a form field. This is needed among other places for
-    bootstrap 3, which needs a 'form-control' class in the field itself
-    """
-    return field.as_widget(attrs={"class": css})
-
-
-@register.filter(name='is_checkbox')
-def is_checkbox(field):
-    """
-    Tests if a field element is a checkbox, as it needs to be handled slightly different
-
-    :param field: a form field
-    :return: boolen
-    """
-    return isinstance(field.field.widget, CheckboxInput)
-
-
-@register.filter(name='is_multiple')
-def is_multiple(field):
-    """
-    Tests if a field element is a multiple select rendered as a checkbox, as it
-    needs to be handled slightly different
-
-    :param field: a form field
-    :return: boolen
-    """
-    return isinstance(field.field.widget, BootstrapSelectMultiple) \
-        or isinstance(field.field.widget, BootstrapSelectMultipleTranslatedOriginal)
-
-
-@register.filter(name='is_fileupload')
-def is_fileupload(field):
-    """
-    Tests if a field element is a file upload, as it needs to be handled slightly different
-
-    :param field: a form field
-    :return: boolen
-    """
-    return isinstance(field.field.widget, ClearableFileInput)
-
-
-@register.inclusion_tag('tags/render_form_element.html')
-def render_form_field(field):
-    """
-    Renders a form field with all necessary labels, help texts and labels
-    within 'form-group'.
-
-    See bootstrap documentation for details: http://getbootstrap.com/css/#forms
-    """
-
-    return {'field': field}
-
-
-@register.inclusion_tag('tags/render_form_submit.html')
-def render_form_submit(save_text='Save', button_class='default'):
-    """
-    Comfort function that renders a submit button with all necessary HTML
-    and CSS
-
-    :param save_text: the text to use on the submit button
-    :param button_class: CSS class to apply to the button, default 'default'
-    """
-    if button_class in ('default',
-                        'primary',
-                        'success',
-                        'info',
-                        'warning',
-                        'danger',
-                        'link'):
-        button_class = button_class
-    else:
-        button_class = 'default'
-
-    return {'save_text': save_text,
-            'button_class': button_class}
-
-
-@register.inclusion_tag('tags/render_form_errors.html')
-def render_form_errors(form):
-    """
-    Renders the non-field errors of a form with all necessary HTML and CSS
-    (non-field errors refer to errors that can't be associated to any single
-    field)
-
-    :param form: the form object
-    """
-    return {'form': form}
-
-
-@register.inclusion_tag('tags/render_form_fields.html')
-def render_form_fields(form, submit_text='Save', show_save=True):
-    """
-    Comfort function that renders all fields in a form, as well as the submit
-    button
-
-    Internally it simply calls the other table_form_* functions and will render
-    the fields in the order they were defined. If you want to change this, you
-    need to call table_form_field for each field yourself. This function will
-    also render a hidden field with a CSRF token, so be sure to pass it to the
-    template.
-
-    It is still necessary to enclose its output in <form> tags!
-
-    :param form: the form to be rendered
-    :param save_text: the text to use on the submit button
-    """
-
-    return {'form': form,
-            'show_save': show_save,
-            'submit_text': submit_text}
