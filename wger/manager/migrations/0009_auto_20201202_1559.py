c6b717c9908c22b24d3f636fb5fd56568752056c
Roland Geider <roland@geider.net>
2020-12-02 16:29:16 +0100

    Fix linting
    Why can't the super linter learn to ignore the migrations? 😤

--- wger/manager/migrations/0009_auto_20201202_1559.py
+++ wger/manager/migrations/0009_auto_20201202_1559.py
@@ -13,11 +13,18 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='setting',
             name='rir',
-            field=models.DecimalField(blank=True, choices=[(None, '------'), (0, 0), (0.5, 0.5), (1, 1), (1.5, 1.5), (2, 2), (2.5, 2.5), (3, 3), (3.5, 3.5), (4, 4)], decimal_places=1, max_digits=3, null=True, verbose_name='RiR'),
+            field=models.DecimalField(
+                blank=True,
+                choices=[(None, '------'), (0, 0), (0.5, 0.5), (1, 1), (1.5, 1.5), (2, 2),
+                         (2.5, 2.5), (3, 3), (3.5, 3.5), (4, 4)],
+                decimal_places=1, max_digits=3, null=True, verbose_name='RiR'),
         ),
         migrations.AddField(
             model_name='workoutlog',
             name='rir',
-            field=models.DecimalField(blank=True, choices=[(None, '------'), (0, 0), (0.5, 0.5), (1, 1), (1.5, 1.5), (2, 2), (2.5, 2.5), (3, 3), (3.5, 3.5), (4, 4)], decimal_places=1, max_digits=3, null=True, verbose_name='RiR'),
+            field=models.DecimalField(
+                blank=True, choices=[(None, '------'), (0, 0), (0.5, 0.5), (1, 1), (1.5, 1.5),
+                                     (2, 2), (2.5, 2.5), (3, 3), (3.5, 3.5), (4, 4)],
+                decimal_places=1, max_digits=3, null=True, verbose_name='RiR'),
         ),
     ]
