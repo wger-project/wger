01c73b4e51111afadf56f3b8874a6e8c21d63abf
Roland Geider <roland@geider.net>
2020-12-08 21:39:34 +0100

    Remove unused import

--- wger/manager/api/views.py
+++ wger/manager/api/views.py
@@ -15,9 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
 
-# Standard Library
-import datetime
-
 # Third Party
 from rest_framework import viewsets
 from rest_framework.decorators import action
