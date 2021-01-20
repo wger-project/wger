dab1c71dbbebd8697c952f2eb3280a3308f2a7c3
Roland Geider <roland@geider.net>
2021-01-20 12:18:51 +0100

    Sort list of IDs

--- wger/exercises/tests/test_exercise_base.py
+++ wger/exercises/tests/test_exercise_base.py
@@ -45,10 +45,10 @@ class ExerciseBaseTestCase(WgerTestCase):
         # Even if these exercises have the same base, only the variations for
         # their respective languages are returned.
         exercise = Exercise.objects.get(pk=81)
-        self.assertListEqual([i.id for i in exercise.variations], sorted([3, 35, 81]))
+        self.assertListEqual(sorted([i.id for i in exercise.variations]), sorted([3, 35, 81]))
 
         exercise2 = Exercise.objects.get(pk=84)
-        self.assertEqual([i.id for i in exercise2.variations], sorted([84, 91, 111, 126]))
+        self.assertEqual(sorted([i.id for i in exercise2.variations]), sorted([84, 91, 111, 126]))
 
     def test_images(self):
         """Test that the correct images are returned for the exercises"""
