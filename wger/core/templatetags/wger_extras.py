667af77dbc77d926c861e3e5fb05ef0fc2236d00
Roland Geider <roland@geider.net>
2022-04-22 13:49:28 +0200

    Fix import for iterable, this doesn't work on 3.10 anymore

--- wger/core/templatetags/wger_extras.py
+++ wger/core/templatetags/wger_extras.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 
 # Standard Library
-from collections import Iterable
+from collections.abc import Iterable
 
 # Django
 from django import template
