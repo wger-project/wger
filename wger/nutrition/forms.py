d5e9332c793ef9bc46b0505d2a30c687814b5786
jiany18 <60591522+jiany18@users.noreply.github.com>
2022-05-05 20:51:27 -0500

    add units

--- wger/nutrition/forms.py
+++ wger/nutrition/forms.py
@@ -101,6 +101,7 @@ class BmiForm(forms.ModelForm):
     weight = forms.DecimalField(
         widget=Html5NumberInput(),
         max_value=999,
+        label=_('Weight (kg)'),
     )
 
     class Meta:
