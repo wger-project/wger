39678c3c60e166078d9595a6e034895bf0f25218
Rohan Karan <rohankaran001@gmail.com>
2022-10-13 01:42:08 +0530

    adding settings to extras/docker/development/settings.py

--- extras/docker/development/settings.py
+++ extras/docker/development/settings.py
@@ -117,13 +117,8 @@ COMPRESS_ROOT = STATIC_ROOT
 EMAIL_PAGE_DOMAIN = 'http://localhost/'
 
 
-#
 # Django Axes
-#
-AXES_ENABLED = True  # allow to disable axes entirely
-# (e.g. if this is run in a local network or so we would save up some resources),
-# but default it true
+AXES_ENABLED = True  # allow to disable axes entirely (e.g. if this is run in a local network or so we would save up some resources), but default is true
 AXES_FAILURE_LIMIT = 5  # configurable, default is 5
 AXES_COOLOFF_TIME = 0.5  # configurable, default is 0.5 hours
-AXES_HANDLER = 'axes.handlers.cache.AxesCacheHandler'  # Configurable,
-# but default is the cache handler.
+AXES_HANDLER = 'axes.handlers.cache.AxesCacheHandler'  # Configurable, but default is the cache handler
