787fd1ed5fb253126c2f53ce4f93a7df559c620a
Roland Geider <roland@geider.net>
2023-03-17 12:37:13 +0100

    Remove more unused classes

--- wger/utils/helpers.py
+++ wger/utils/helpers.py
@@ -37,7 +37,6 @@ from django.utils.http import (
     urlsafe_base64_encode,
 )
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -215,37 +214,9 @@ def normalize_decimal(d):
     normalized = d.normalize()
     sign, digits, exponent = normalized.as_tuple()
     if exponent > 0:
-        return decimal.Decimal((sign, digits + (0, ) * exponent, 0))
+        return decimal.Decimal((sign, digits + (0,) * exponent, 0))
     else:
         return normalized
-
-
-def levenshtein(s1, s2):
-    """
-    The Levenshtein distance
-
-    This is used as a string metric for measuring the difference between
-    two sequences. We use this to determine whether or not similar
-    exercises have been added
-
-    Source: Wikipedia
-
-    :param input: two input strings
-    :return: number of characters different between the strings
-    """
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
-
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2 + 1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
 
 
 def random_string(length=32):
