5c9c99e38a0b8b5c8ce701402ff1bbbab8d6799f
Roland Geider <roland@geider.net>
2023-07-27 17:06:41 +0200

    Comment out debug toolbar url

--- wger/urls.py
+++ wger/urls.py
@@ -305,4 +305,4 @@ urlpatterns += [
 #
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
+    # urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
