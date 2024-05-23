e3214eb7456e98a79c22825af53cfed7c0ee3288
Roland Geider <roland@geider.net>
2024-05-23 17:40:00 +0200

    Consolidate fiber related migrations into one

--- wger/nutrition/migrations/0021_add_fibers_field.py
+++ wger/nutrition/migrations/0021_add_fibers_field.py
@@ -16,7 +16,12 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='nutritionplan',
-            name='goal_fibers',
+            name='goal_fiber',
             field=models.IntegerField(default=None, null=True),
+        ),
+        migrations.RenameField(
+            model_name='ingredient',
+            old_name='fibres',
+            new_name='fiber',
         ),
     ]
