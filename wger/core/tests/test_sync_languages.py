ee9328667f522938c0cc31328b32a4e6a364ff17
Victor <vnacher@thinkdeep.ai>
2024-06-13 18:58:54 +0200

    fix tests

--- wger/core/tests/test_sync_languages.py
+++ wger/core/tests/test_sync_languages.py
@@ -27,12 +27,6 @@ mock_json_data = [
             {"model": "core.language", "pk": 2, "fields": {"short_name": "en", "full_name": "English", "full_name_en": "English"}},
         ]
 
-for lang in mock_json_data:
-    language, created = Language.objects.update_or_create(
-        short_name=lang["fields"]["short_name"],
-        defaults={'full_name': lang["fields"]["full_name"], 'full_name_en': lang["fields"]["full_name_en"]},
-    )
-
 
 class LanguageSyncResetTestCase(WgerTestCase):
     """
@@ -46,11 +40,11 @@ class LanguageSyncResetTestCase(WgerTestCase):
         ]
 
         for lang in self.mock_json_data:
-            language, created = self.Language.objects.update_or_create(
+            language, created = Language.objects.update_or_create(
                 short_name=lang["fields"]["short_name"],
                 defaults={'full_name': lang["fields"]["full_name"], 'full_name_en': lang["fields"]["full_name_en"]},
             )
-
+        call_command('loaddata', 'languages.json')
         self.temp_json_file = "temp_languages.json"
         with open(self.temp_json_file, "w") as f:
             json.dump(self.mock_json_data, f)
@@ -65,7 +59,8 @@ class LanguageSyncResetTestCase(WgerTestCase):
         """
         self.assertEqual(f'{Language.objects.get(pk=1)}', 'Deutsch (de)')
         Language.objects.filter(id=1).delete()
-        self.assertRaises(Language.DoesNotExist, f'{Language.objects.get(pk=1)}')
+
+        self.assertRaises(Language.DoesNotExist, lambda: Language.objects.get(pk=1))
 
         call_command('sync_core_languages')
 
