3d6e44c711ac4585bd032a408aa25711a24c0575
Roland Geider <roland@geider.net>
2025-02-23 14:10:31 +0100

    Only import pymongo when trying to connect to it

--- wger/nutrition/management/commands/import-off-products.py
+++ wger/nutrition/management/commands/import-off-products.py
@@ -18,7 +18,6 @@ import os
 
 # Third Party
 import requests
-from pymongo import MongoClient
 
 # wger
 from wger.core.models import Language
@@ -27,7 +26,6 @@ from wger.nutrition.management.products import (
     Mode,
 )
 from wger.nutrition.off import extract_info_from_off
-
 
 logger = logging.getLogger(__name__)
 
@@ -65,6 +63,13 @@ class Command(ImportProductCommand):
         )
 
     def import_mongo(self, languages: dict[str:int]):
+        try:
+            # Third Party
+            from pymongo import MongoClient
+        except ImportError:
+            self.stdout.write('Please install pymongo, `pip install pymongo`')
+            return
+
         client = MongoClient('mongodb://off:off-wger@127.0.0.1', port=27017)
         db = client.admin
         for product in db.products.find({'lang': {'$in': list(languages.keys())}}):
@@ -128,13 +133,6 @@ class Command(ImportProductCommand):
             tmp_folder.cleanup()
 
     def handle(self, **options):
-        try:
-            # Third Party
-            from pymongo import MongoClient
-        except ImportError:
-            self.stdout.write('Please install pymongo, `pip install pymongo`')
-            return
-
         if options['mode'] == 'insert':
             self.mode = Mode.INSERT
 
