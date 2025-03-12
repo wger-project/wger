64764e16a3d4dec5a3342d196f08686387b3a988
Roland Geider <roland@geider.net>
2025-03-12 20:49:56 +0100

    Remove duplicate logger
    This was clogging the console anyway

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -23,7 +23,6 @@ from datetime import timedelta
 from wger import get_version
 from wger.utils.constants import DOWNLOAD_INGREDIENT_WGER
 
-
 """
 This file contains the global settings that don't usually need to be changed.
 For a full list of options, visit:
@@ -291,13 +290,8 @@ LOGGING = {
     'loggers': {
         'wger': {
             'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-        '': {
-            'handlers': ['console'],
             'level': 'INFO',
-            'propagate': False,
-        }
+        },
     }
 }
 
