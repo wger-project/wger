d88eebe4732a422b9b2c5518f084f97f4ef2ae6a
Roland Geider <roland@geider.net>
2025-06-26 10:57:25 +0200

    Add start and end fields to the nutritional plan models

--- wger/nutrition/api/serializers.py
+++ wger/nutrition/api/serializers.py
@@ -316,6 +316,8 @@ class NutritionPlanSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'creation_date',
+            'start',
+            'end',
             'description',
             'only_logging',
             'goal_energy',
