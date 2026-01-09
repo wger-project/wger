c6412e96d5ab31b1fa84522545f7fcc55b784bdc
Francisco <112600427+franchsli@users.noreply.github.com>
2026-01-09 16:37:31 -0500

    style: format urls.py file

--- wger/urls.py
+++ wger/urls.py
@@ -297,7 +297,7 @@ urlpatterns = i18n_patterns(
 # URLs without language prefix
 #
 urlpatterns += [
-    path("i18n/", include("django.conf.urls.i18n")),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('robots.txt', TextTemplateView.as_view(template_name='robots.txt'), name='robots'),
     # API
     path('api/v2/exercise/search/', exercises_api_views.search, name='exercise-search'),
