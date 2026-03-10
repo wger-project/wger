3590c5851f9497abef5eafcef96047753e3a8b0c
MadoctheHadoc <madockarthaus@hotmail.com>
2026-03-11 00:24:38 +0100

    Automatic reading of image dates.
    Automatic reading of dates when images are uploaded to the gallery
    
    The date field in the input form is made not required and if no date is available in the EXIF meta data and no date is given, it will simply default to the current date.

--- wger/gallery/forms.py
+++ wger/gallery/forms.py
@@ -10,9 +10,12 @@ from wger.gallery.models import Image
 from wger.utils.constants import DATE_FORMATS
 from wger.utils.widgets import Html5DateInput
 
+# Python Imaging Library to extract data
+from PIL import Image as PilImage
+from datetime import datetime
 
 class ImageForm(ModelForm):
-    date = DateField(input_formats=DATE_FORMATS, widget=Html5DateInput())
+    date = DateField(input_formats=DATE_FORMATS, widget=Html5DateInput(), required=False)
 
     class Meta:
         model = Image
@@ -20,3 +23,29 @@ class ImageForm(ModelForm):
         widgets = {
             'user': widgets.HiddenInput(),
         }
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        # Tries to extract date from EXIF metadata
+        if 'image' in self.changed_data:
+            exif_date = self._get_exif_date(self.cleaned_data['image'])
+            if exif_date:
+                instance.date = exif_date
+
+        if commit:
+            instance.save()
+        return instance
+
+    def _get_exif_date(self, image_file):
+        try:
+            img = PilImage.open(image_file)
+            exif_data = img._getexif()
+            if exif_data:
+                # EXIF constant index for date and datetime format
+                date_str = exif_data.get(36867)
+                if date_str:
+                    return datetime.strptime(date_str, '%Y:%m:%d %H:%M:%S').date()
+        except Exception as e:
+            pass
+        return None
