aac0b55dde54ad4d3b910740f3cc3efff9e0b2ba
Roland Geider <roland@geider.net>
2026-03-28 14:11:52 +0100

    Improve migration
    We preload all the needed objects and perform the updates in bulk

--- wger/core/migrations/0022_move_email_verified_to_emailaddress.py
+++ wger/core/migrations/0022_move_email_verified_to_emailaddress.py
@@ -1,24 +1,30 @@
-from django.db import migrations
+from django.db import migrations, IntegrityError
+from django.db.migrations.state import StateApps
+
 
 """
 Migration script to copy 'email_verified' column of UserProfile to 'verified' column of EmailAddress table
 """
 
-def migrate_verification_status(apps, schema_editor):
+
+def migrate_verification_status(apps: StateApps, schema_editor):
+    # Bulk-create entries for the allauth table using existing userprofile fields
     UserProfile = apps.get_model('core', 'UserProfile')
     EmailAddress = apps.get_model('account', 'EmailAddress')
 
-    for profile in UserProfile.objects.all():
-        if profile.user.email:
-            # Create allauth table using existing userprofile fields
-            EmailAddress.objects.get_or_create(
-                user=profile.user,
-                email=profile.user.email,
-                defaults={
-                    'verified': profile.email_verified,
-                    'primary': True
-                }
-            )
+    profiles = UserProfile.objects.select_related('user').exclude(user__email='')
+    email_addresses = [
+        EmailAddress(
+            user=profile.user,
+            email=profile.user.email,
+            verified=profile.email_verified,
+            primary=True,
+        )
+        for profile in profiles
+    ]
+
+    EmailAddress.objects.bulk_create(email_addresses, ignore_conflicts=True)
+
 
 class Migration(migrations.Migration):
     dependencies = [
