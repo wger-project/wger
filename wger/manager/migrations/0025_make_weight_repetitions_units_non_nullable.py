24f7b27615b2782f490ef5a55d4c3200b95450d2
pankaj-basnet <165250380+pankaj-basnet@users.noreply.github.com>
2026-04-08 13:27:54 +0545

    fix(manager): make weight and repetition units mandatory (SlotEntry and WorkoutLog tables)
    - data migration to backfill null units with defaults already done.
    - Update SlotEntry and WorkoutLog models to non-nullable ForeignKeys.

---
+++ wger/manager/migrations/0025_make_weight_repetitions_units_non_nullable.py
@@ -0,0 +1,56 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('manager', '0024_fill_null_weight_repetitions_units'),
+        ('core', '0022_move_email_verified_to_emailaddress.py'),
+    ]
+
+    operations = [
+        # SlotEntry: make repetition_unit non-nullable
+        migrations.AlterField(
+            model_name='slotentry',
+            name='repetition_unit',
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to='core.repetitionunit',
+            ),
+        ),
+        # SlotEntry: make weight_unit non-nullable
+        migrations.AlterField(
+            model_name='slotentry',
+            name='weight_unit',
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to='core.weightunit',
+                verbose_name='Unit',
+            ),
+        ),
+        # WorkoutLog: make repetition_unit non-nullable
+        migrations.AlterField(
+            model_name='workoutlog',
+            name='repetitions_unit',
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to='core.repetitionunit',
+                verbose_name='Repetitions unit',
+            ),
+        ),
+        # WorkoutLog: make weight_unit non-nullable
+        migrations.AlterField(
+            model_name='workoutlog',
+            name='weight_unit',
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to='core.weightunit',
+                verbose_name='Weight unit',
+            ),
+        ),
+    ]
